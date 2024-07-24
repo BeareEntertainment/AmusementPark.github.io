cd5e84b2b5c4f401ed103bb53d464c53eb4476b9
BeareEntertainment <bearentertainment4@gmail.com>
2024-07-24 19:11:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const navLinks = document.querySelector('.nav-links');
 
